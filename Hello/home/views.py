e012514a3fca9a36e7dc0c098b5fa8e936ae0237
gagangupta12 <125540244+gagangupta12@users.noreply.github.com>
2023-08-13 16:49:06 +0530

    Add files via upload

---
+++ Hello/home/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render , HttpResponse
+
+# Create your views here.
+from django.template import loader 
+def index(request):
+    return render(request, 'index.html', {}) 
+
+def about(request):
+    return HttpResponse("this is about")
+
+def services(request):
+    return HttpResponse("this is services")
+
+def contact(request):
+    return HttpResponse("this is contact")
